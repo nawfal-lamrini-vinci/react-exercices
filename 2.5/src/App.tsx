779df2f90b52d08c017561e7e5c65d9752939280
nawfal-lamrini-vinci <nawfal.lamrini@student.vinci.be>
2025-11-13 17:45:03 +0100

    new:ex2.5-destructuring

--- 2.5/src/App.tsx
+++ 2.5/src/App.tsx
@@ -3,7 +3,10 @@ import ClickCounter from "./ClickCounter";
 const App = () => {
   return (
     <div>
-      <ClickCounter />
+      <ClickCounter
+        title="Counter"
+        message="You are a master in the art of clicking !."
+      />
     </div>
   );
 };
--- 2.5/src/ClickCounter.tsx
+++ 2.5/src/ClickCounter.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
-export default function ClickCounter() {
+interface counterProps {
+  title: string;
+  message: string;
+}
+
+export default function ClickCounter({ title, message }: counterProps) {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <h1>{title}</h1>
       <div onClick={() => setCount(count + 1)} className="div-counter">
         <p>Counter</p>
         <p>{count}</p>
       </div>
+      <p>{count >= 10 ? message : ""}</p>
     </div>
   );
 }
