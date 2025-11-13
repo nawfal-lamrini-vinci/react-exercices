d9acf423a3f3eecb3917c4c175a52661ef349e6f
nawfal-lamrini-vinci <nawfal.lamrini@student.vinci.be>
2025-11-13 17:56:36 +0100

    new:ex2.5-events

--- 2.5/src/App.tsx
+++ 2.5/src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
       <ClickCounter
         title="Counter"
         message="You are a master in the art of clicking !."
+        warning="Please click on me now !."
       />
     </div>
   );
--- 2.5/src/ClickCounter.tsx
+++ 2.5/src/ClickCounter.tsx
@@ -3,18 +3,30 @@ import { useState } from "react";
 interface counterProps {
   title: string;
   message: string;
+  warning: string;
 }
 
-export default function ClickCounter({ title, message }: counterProps) {
+export default function ClickCounter({
+  title,
+  message,
+  warning,
+}: counterProps) {
   const [count, setCount] = useState(0);
+  const [focus, setFocus] = useState(false);
 
   return (
     <div>
       <h1>{title}</h1>
-      <div onClick={() => setCount(count + 1)} className="div-counter">
+      <div
+        onMouseEnter={() => setFocus(true)}
+        onMouseLeave={() => setFocus(false)}
+        onClick={() => setCount(count + 1)}
+        className="div-counter"
+      >
         <p>Counter</p>
         <p>{count}</p>
       </div>
+      {focus && <p>{warning}</p>}
       <p>{count >= 10 ? message : ""}</p>
     </div>
   );
